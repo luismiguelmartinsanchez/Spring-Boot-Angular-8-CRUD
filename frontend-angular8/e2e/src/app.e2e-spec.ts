444711898262e87716cb6d9d5da61ac19cdf85ec
Luis Miguel Martín <lm.martin@preving.com>
2020-10-26 10:57:53 +0100

    FIXED - Más cambios de nombre en el proyecto

--- frontend-angular8/e2e/src/app.e2e-spec.ts
+++ frontend-angular8/e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('angular8-springboot-client app is running!');
+    expect(page.getTitleText()).toEqual('frontend-angular app is running!');
   });
 
   afterEach(async () => {
--- frontend-angular8/src/app/app.component.spec.ts
+++ frontend-angular8/src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'angular8-springboot-client'`, () => {
+  it(`should have as title 'frontend-angular'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('angular8-springboot-client');
+    expect(app.title).toEqual('frontend-angular');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('angular8-springboot-client app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('frontend-angular app is running!');
   });
 });
